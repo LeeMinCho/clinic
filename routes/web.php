4cd389fe90fa66e0bab9a4a860ca210bb9d8c503
LeeMinCho <ekayunan98@gmail.com>
2021-10-01 23:26:45 +0700

    Make Login Page and Dashboard Page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
